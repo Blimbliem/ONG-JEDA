1d9d10e033840201e976014703d0ca49f8c5ff01
NicBarreira <nicode021@gmail.com>
2025-08-18 19:54:34 -0300

    Inicio da pagina de projetos

---
+++ front_end/projetos/script.js
@@ -0,0 +1,22 @@
+// navbar/navbar.js
+document.addEventListener("DOMContentLoaded", () => {
+  const container = document.getElementById("navbar-container");
+
+  // Carrega o HTML da navbar
+  fetch("../navbar/navbar.html")
+  
+    .then(response => response.text())
+    .then(data => {
+      console.log("Navbar carregada com sucesso!")
+      container.innerHTML = data;
+
+      // Depois que o HTML foi carregado, adiciona funcionalidade do toggle
+      const toggle = container.querySelector(".navbar-toggle");
+      const menu = container.querySelector(".navbar-menu");
+
+      toggle.addEventListener("click", () => {
+        menu.classList.toggle("active");
+      });
+    })
+    .catch(error => console.error("Erro ao carregar navbar:", error));
+});
